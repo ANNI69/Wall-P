4d817217d69c757c019807890a37d7b34c17f454
Anii <84516362+ANNI69@users.noreply.github.com>
2022-09-26 19:58:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function Resolution() {
+    alert("Screen resolution is: " + screen.width + " x " + screen.height);
+}
+document.getElementById("Serch").value = search;
+document.write(search);
+document.getElementById("Serch").value = search;
+
+function getname() {
+    var name = document.getElementById("Serch").value;
+    document.getElementById("name").value = name;
+}
